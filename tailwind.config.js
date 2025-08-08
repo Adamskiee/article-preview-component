7a6f77bfd572b769c72aa55176e1cdde5f7d4829
Adamskiee <tosinomatthew@gmail.com>
2025-08-08 16:07:11 +0800

    Enhance card component styles for better responsiveness and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./build/*.html"],
   theme: {
     extend: {
+      screens: {
+        "desktop": "1439px"
+      },
       colors: {
         "dark-grayish-blue": "var(--dark-grayish-blue)",
         "dark-blue": "var(--dark-blue)",
